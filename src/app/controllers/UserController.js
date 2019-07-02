2f4cc9562236d5135139c9fca1c452664a28a69f
Henrique Suel <Henrique.Suel@tembici.local>
2019-07-02 15:12:43 -0300

    feat: controller validation email

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,15 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // Validação de email
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
