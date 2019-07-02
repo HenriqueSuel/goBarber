175b6cbb7f4846589281c7df742f1430d44c0c0e
Henrique Suel <Henrique.Suel@tembici.local>
2019-07-02 18:10:40 -0300

    finalizado Iniciando back-end do GoBarber

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
+// User
 routes.post('/users', UserController.store);
+
+// sessions
 routes.post('/sessions', SessionController.store);
+
+// Router Authorization
+routes.use(authMiddlewares);
+
+routes.put('/users', UserController.update);
 export default routes;
