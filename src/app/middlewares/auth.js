5f7b2e938fde81f6ad949ea00b9f015235f04199
Henrique Suel <Henrique.Suel@tembici.local>
2019-08-21 17:35:41 -0300

    feat: create Api apointmentController

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,6 +4,7 @@ import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provided' });
   }
@@ -12,7 +13,9 @@ export default async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
     req.userId = decoded.id;
+
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token not invalid' });
--- src/routes.js
+++ src/routes.js
@@ -7,26 +7,30 @@ import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-// User
+// sessions
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+// Router Authorization
+routes.use(authMiddlewares);
+
+// User
 routes.put('/users', UserController.update);
 
 // Providers
 routes.get('/providers', ProviderController.index);
 
-// sessions
-routes.post('/sessions', SessionController.store);
+// Calendario
+routes.post('/appointments', AppointmentController.store);
 
 // Foto
 routes.post('/files', upload.single('file'), FileController.store);
-
-// Router Authorization
-routes.use(authMiddlewares);
 
 export default routes;
