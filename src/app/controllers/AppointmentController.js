3ffc2f64271c254c9776ac56edba67aa611b7285
Henrique Suel <Henrique.Suel@tembici.local>
2019-08-22 16:05:36 -0300

    feat: send email user

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -106,6 +106,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+        },
       ],
     });
 
@@ -130,7 +135,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' HH:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
