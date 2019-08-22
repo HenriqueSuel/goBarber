5fae9f31aee6cdbc7e3fbfb9712f6e17432d8f86
Henrique Suel <Henrique.Suel@tembici.local>
2019-08-22 14:51:28 -0300

    configurando email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '06c66385a14b28',
+    pass: '991acb6087d931',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
